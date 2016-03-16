07b3de7c52d8b374219c57d1e0b41f939d00e594
will459 <william.correia@protonmail.ch>
2016-03-16 10:13:13 -0400

    Added names to login.jspx form elements so they can be retrieved.  Added Login.java servlet to handle login tasks.

---
+++ src/accessctrl/Login.java
@@ -0,0 +1,43 @@
+package accessctrl;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Login
+ */
+@WebServlet("/Login")
+public class Login extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Login() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//Should never enter via get
+		//If it entered via get assume it's not a login request, redirect to home.
+		response.sendRedirect("home.html");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("Hit the servlet");
+		System.out.println(request.getParameter("username"));
+		System.out.println(request.getParameter("password"));
+	}
+
+}
