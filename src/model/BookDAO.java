59d3dbcb78dbc7e1824f7e7e150e954f7766ce4d
BasitAbdul <abasit_7@hotmail.com>
2016-03-18 00:03:02 -0400

    Last update did not push the DAO file for some reason. In this update it does

---
+++ src/model/BookDAO.java
@@ -0,0 +1,65 @@
+/**
+ * 
+ */
+package model;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+/**
+ * @author Basit
+ *
+ */
+public class BookDAO {
+	
+private DataSource ds;
+	
+	public BookDAO() throws ClassNotFoundException {
+		// TODO Auto-generated constructor stub
+		
+		try
+		{
+			ds = (DataSource) (new InitialContext()).lookup("java:/comp/env/jdbc/EECS");
+		} 
+		
+		catch (NamingException e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	public Map<String, BookBean> retrieve(String name) throws
+	SQLException{
+		String query = "select * from enrollment where cid like '%" + name +"%';";
+		
+		Map<String, BookBean> rv = new HashMap<String, BookBean>();
+		Connection con = this.ds.getConnection();
+		PreparedStatement p = con.prepareStatement(query);
+		ResultSet r = p.executeQuery();
+		
+		while (r.next()){
+			String bidd = r.getString("BID");
+			String title = r.getString("TITLE");
+			int price = r.getInt("PRICE");
+			String category = r.getString("CATEGORY");
+			
+			BookBean book = new BookBean(bidd, title, category, price);
+			rv.put(bidd, book);
+		}
+		r.close();
+		p.close();
+		con.close();
+		return rv;
+	}
+
+}
